f515824511767b5edbca67a54be1b9abf931a16b
unknown <garyaguilar300@gmail.com>
2021-09-15 08:36:23 -0400

    Suma de los elementos de un vector con recursividad

---
+++ src/Ejercicio.java
@@ -0,0 +1,59 @@
+public class Ejercicio {
+
+    public static int factorial (int n){
+        int factorial;
+
+        if (n == 1){
+            //caso base
+            factorial = n;
+        }else{
+            factorial = n * factorial(n - 1);
+        }
+
+        return factorial;
+    }
+
+
+    /*
+    Maximo comun divisor (mcd), algoritmo de euclides
+    a = 412 y b = 184
+    * */
+
+    public static int mcd(int a, int b){
+        int mcd;
+
+        if (a == b){
+            //caso base
+            mcd = b;
+        }else{
+            if (a > b){
+                a = a - b;
+            }else{
+                b = b - a;
+            }
+
+            mcd = mcd(a, b);
+        }
+
+        return mcd;
+    }
+
+    /*
+    Suma de los elementos de un vector
+    vector = {2, 4, 6}
+    suma = 2 + {4, 6}
+    suma 4 + {6}
+    suma = 6 (caso base)
+     */
+
+    public static int sumaVector(int[] vector, int indice){
+        int suma;
+
+        if(indice == vector.length - 1){
+            suma = vector[indice];
+        }else {
+            suma = vector[indice] + sumaVector(vector, indice +1);
+        }
+        return suma;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println(Ejercicio.factorial(5));*/
+        //System.out.println(Ejercicio.mcd(412,184));
+        int[] vector = {2, 4, 6};
+        System.out.println(Ejercicio.sumaVector(vector, 0));
+    }
+}
